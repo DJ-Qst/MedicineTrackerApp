57ce20f8a786fda9996e8bc34ab4ed988e1abebd
Jorden Quast <jman7049@icloud.com>
2021-09-10 08:36:04 -0500

    Added options to FAB and created plain pages for the forms

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'medicine_page.dart';
 import 'sub_pages.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 /*
 How it works:
@@ -10,14 +11,14 @@ How it works:
  */
 
 void main() => runApp(MaterialApp(
-  //TODO: Make a theme for the app
-  // theme: ThemeData()
-  home: App(medsList: [
-    {"name": "Tylenol", "dosage": "2 Pills", "separation": "6 hr"},
-    {"name": "Ibuprofin", "dosage": "2 Pills", "separation": "6 hr"},
-    {"name": "NyQuil", "dosage": "30 ml", "separation": "4 hr"},
-  ]),
-));
+      //TODO: Make a theme for the app
+      // theme: ThemeData()
+      home: App(medsList: [
+        {"name": "Tylenol", "dosage": "2 Pills", "separation": "6 hr"},
+        {"name": "Ibuprofin", "dosage": "2 Pills", "separation": "6 hr"},
+        {"name": "NyQuil", "dosage": "30 ml", "separation": "4 hr"},
+      ]),
+    ));
 
 // TODO: Read and Write list of Maps to a file for saving and loading medicines
 
@@ -37,40 +38,69 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        // This TabController is voodoo magic that makes the tabBar work
-        length: 2,
-        child: Scaffold(
-          // Scaffold holds the entire app including the appBar, tabBar, Body, and FAB
-          appBar: AppBar(
-            // TODO: Add settings button and have settings page
-            title: Text("Medicine Tracker"),
-            actions: <Widget>[  // List of widgets for actions in the appBar
-              // Settings Page button
-              IconButton(
-                onPressed: () {print("Settings Button Clicked!");},
-                icon: Icon(Icons.settings),
-                tooltip: "Open Settings",
-              )
+      // This TabController is voodoo magic that makes the tabBar work
+      length: 2,
+      child: Scaffold(
+        // Scaffold holds the entire app including the appBar, tabBar, Body, and FAB
+        appBar: AppBar(
+          // TODO: Add settings button and have settings page
+          title: Text("Medicine Tracker"),
+          actions: <Widget>[
+            // List of widgets for actions in the appBar
+            // Settings Page button
+            IconButton(
+              onPressed: () {
+                print("Settings Button Clicked!");
+              },
+              icon: Icon(Icons.settings),
+              tooltip: "Open Settings",
+            )
+          ],
+          bottom: const TabBar(
+            // At the bottom of the AppBar there is two icons showing the tabs of the app, very positional
+            tabs: [
+              Tab(icon: Icon(Icons.local_hospital_outlined)), // Medicine Tab
+              Tab(icon: Icon(Icons.accessibility)), // People Tab
             ],
-            bottom: const TabBar(
-              // At the bottom of the AppBar there is two icons showing the tabs of the app, very positional
-              tabs: [
-                Tab(icon: Icon(Icons.local_hospital_outlined)), // Medicine Tab
-                Tab(icon: Icon(Icons.accessibility)), // People Tab
-              ],
-            ),
           ),
+        ),
 
-          body: TabBarView(
-              // The Pages for each of the tabs, also very positional
-              children: [
-                MedicinePage(medicines: meds), // Medicine Tab
-                Icon(Icons.accessibility)
-              ] // People Tab
-              ),
+        body: TabBarView(
+            // The Pages for each of the tabs, also very positional
+            children: [
+              MedicinePage(medicines: meds), // Medicine Tab
+              Icon(Icons.accessibility)
+            ] // People Tab
+            ),
 
-          // TODO: Make FAB give options for which form to pick
-          floatingActionButton: FloatingActionButton(
+        floatingActionButton: SpeedDial(
+          spacing: 3,
+          icon: Icons.add,
+          activeIcon: Icons.close,
+          overlayOpacity: 0,
+          curve: Curves.easeIn,
+          children: [
+            SpeedDialChild(
+              child: Icon(Icons.person_add),
+              label: "Add Person",
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return PersonForm();
+                  }));
+                }
+            ),
+            SpeedDialChild(
+                child: Icon(Icons.healing),
+                label: "Add Medicine",
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return MedicineForm();
+                  }));
+                }),
+          ],
+        ),
+
+        /*FloatingActionButton(
             // Floating action button that will give the options for creating medicine or people
             onPressed: () => setState(() {
               Navigator.push(context, MaterialPageRoute(builder: (context) {
@@ -85,8 +115,8 @@ class _AppState extends State<App> {
               print("Number of Medicines: ${meds.length}");
             }),
             child: Icon(Icons.add),
-          ),
-        ),
-      );
+          ),*/
+      ),
+    );
   }
 }
